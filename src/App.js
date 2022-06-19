b21da8b0247b7252fbb7fd05a32b30a509e937ed
hariyono1995 <onnelikrio@gmail.com>
2022-06-19 19:32:19 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
         ))
       ) : (
         <div className="empty">
-          <h2>No movie found</h2>
+          <h2>Movie not found</h2>
         </div>
       )}
     </div>
